196c9d34a475623d58f1d57aa8d1c830f3b77b63
Angelo Pool Rodriguez <brofan66@gmail.com>
2020-12-28 20:20:14 -0500

    Updates

---
+++ precache-manifest.61af6943cd2d069c4b061e562d188cb5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e5cc70086a5e6f6536a86efb80739f",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "436f55899eb00d21fa21",
+    "url": "/Drum-Machine/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "1f4c7247a0c34f9cb4bf",
+    "url": "/Drum-Machine/static/js/2.4c3a52f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drum-Machine/static/js/2.4c3a52f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "436f55899eb00d21fa21",
+    "url": "/Drum-Machine/static/js/main.a3aae817.chunk.js"
+  },
+  {
+    "revision": "3d8833829d412425f337",
+    "url": "/Drum-Machine/static/js/runtime-main.68553a9a.js"
+  },
+  {
+    "revision": "f6500e94a7b9bf7b5d6ae2d09cbc2943",
+    "url": "/Drum-Machine/static/media/bass.f6500e94.mp3"
+  },
+  {
+    "revision": "736be47e514271d2a034d887e78224c2",
+    "url": "/Drum-Machine/static/media/cymbals.736be47e.mp3"
+  },
+  {
+    "revision": "043bebf98568418995f9fae1140a6340",
+    "url": "/Drum-Machine/static/media/flute.043bebf9.wav"
+  },
+  {
+    "revision": "b06be46f8b7acd87b43d82ce5ea9e9a3",
+    "url": "/Drum-Machine/static/media/guitar.b06be46f.mp3"
+  },
+  {
+    "revision": "b06ca26f3f670de2ee2d4aed9b3885f1",
+    "url": "/Drum-Machine/static/media/piano.b06ca26f.mp3"
+  },
+  {
+    "revision": "bb455d41151d3f7c45b7015ad104f7c3",
+    "url": "/Drum-Machine/static/media/saxophone.bb455d41.mp3"
+  },
+  {
+    "revision": "1a5944a38557a992660125ea39818ead",
+    "url": "/Drum-Machine/static/media/tambourine.1a5944a3.wav"
+  },
+  {
+    "revision": "f90da1df6090d8b754dda87e1f195b95",
+    "url": "/Drum-Machine/static/media/trombone.f90da1df.mp3"
+  },
+  {
+    "revision": "95d098b2ae3bc559af30815413d66fb2",
+    "url": "/Drum-Machine/static/media/violin.95d098b2.mp3"
+  }
+]);
